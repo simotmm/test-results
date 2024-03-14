3bb0e843e90b380129feb0355fe484b7a3d0d8fc
simotmm <s333017@studenti.polito.it>
2024-03-14 02:13:17 +0100

    modificato commento

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
-/* Input da tastiera impostato di default.
-   Per impostare le condizioni iniziali modificare le condizioni iniziali dalle "#define" in main.c.
-   Input da riga di comando non ancora implementato. */
+// Input da tastiera impostato di default.
+// Per impostare le condizioni iniziali modificare le condizioni iniziali dalle #define.
+// Input da riga di comando non ancora implementato. 
 
 #include <stdio.h>
 #include <math.h>
