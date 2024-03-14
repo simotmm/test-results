3845353c2b44ea53be06917a4b201aad739c0b5e
simotmm <s333017@studenti.polito.it>
2024-03-14 02:16:06 +0100

    seconda modifica al commento

--- src/main.c
+++ src/main.c
@@ -1,3 +1,7 @@
+// Input from keyboard is set as default.
+// The initial conditions can be modified in the #define section.
+// Command line input to be implemented.
+
 // Input da tastiera impostato di default.
 // Per impostare le condizioni iniziali modificare le condizioni iniziali dalle #define.
 // Input da riga di comando non ancora implementato. 
